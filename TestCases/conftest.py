3dc98ebfff840ce876b615ad03517aced4f190b2
KP-crypto <puranik324@gmail.com>
2020-08-22 21:57:02 +0530

    my third commit

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -2,6 +2,22 @@ from selenium import webdriver
 import pytest
 @pytest.fixture()
 def setup():
-    driver=webdriver.Chrome(executable_path="C:/Users/Acer/Downloads/chromedriver_win32/chromedriver.exe")
+    if browser=="Chrome":
+        driver=webdriver.Chrome(executable_path="C:/Users/Acer/Downloads/chromedriver_win32/chromedriver.exe")
+    elif browser=="firefox":
+        driver=webdriver.Firefox()
+    else:
+        driver=webdriver.Ie()
     return driver
+
+
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")
 
