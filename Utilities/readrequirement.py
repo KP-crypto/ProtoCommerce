3c477ff9b798615d006463ca0dcd305263b25e74
Ketan Puranik <69633773+KP-crypto@users.noreply.github.com>
2022-02-08 18:01:08 +0530

    Update readrequirement.py

--- Utilities/readrequirement.py
+++ Utilities/readrequirement.py
@@ -1,25 +1,39 @@
 import configparser
+import os
+from pathlib import Path
 
+def get_project_root() -> Path:
+    output = Path(__file__).parent.parent
+    output = str(output)
+    result = output.split('\\')
+    path = "/".join(result)
+    return path
+
+path = get_project_root()
 config=configparser.RawConfigParser()
-config.read("C:/Users/Acer/PycharmProjects/ProtoCommerce/Configuration/config.txt")
+config.read(path)
 
 class Readconfig():
     @staticmethod
     def getapplicationURL():
         url=config.get("common info","url")
         return url
+    
     @staticmethod
     def getName():
         Name=config.get("common info","Name")
         return Name
+    
     @staticmethod
     def getEmail():
         Email=config.get("common info","Email")
         return Email
+    
     @staticmethod
     def getPassword():
         Password=config.get("common info","Password")
         return Password
+    
     @staticmethod
     def getDOB():
         DOB=config.read("common info","DOB")
